493d89b0c07d944c80794dafe249978f4394c175
Sundaram Seth <sundaram.s@eccentricengine.com>
2024-07-13 13:58:29 +0530

    connectemango db

--- api/index.js
+++ api/index.js
@@ -1,5 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
 
+dotenv.config();
+
+mongoose.connect(process.env.MANGO).then(
+    ()=>{
+        console.log('MongoDb is connected')
+    }).catch((err)=>{
+        console.log(err)
+    });
 const app = express();
 
 app.listen(3000, ()=>{
