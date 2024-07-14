0dfa604a82e920f1b3698996370904168914aea2
Sundaram Seth <sundaram.s@eccentricengine.com>
2024-07-14 23:15:53 +0530

    create signin apin route

--- api/controllers/auth.controler.js
+++ api/controllers/auth.controler.js
@@ -2,6 +2,10 @@ import { error } from "console";
 import User from "../models/users.model.js";
 import bcryptjs from 'bcryptjs'
 import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv'
+
+dotenv.config();
 
 export const signup = async(req, res, next)=>{
     const {username, email, password} = req.body;
@@ -21,6 +25,39 @@ export const signup = async(req, res, next)=>{
     try{
         await newUser.save();
         res.json('Signup Successful')
+    }
+    catch(error){
+    next(error);
+    }
+
+}
+
+
+export const signin = async(req, res, next)=>{
+    const {email, password} = req.body;
+
+    if(!email || !password || email === '' || password === ''){
+       return next(errorHandler(400, 'All Fields are required'))
+    }
+
+    try{
+        const validUser = await User.findOne({email});
+        if (!validUser){
+          return next(errorHandler(404,'User not found!'));
+        }
+
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if(!validPassword){
+           return next(errorHandler(404,'Invalid Password'));
+        }
+
+        const token = jwt.sign({id:validUser._id}, process.env.JWT_SECRET);
+        
+        const{password:pass, ...rest} = validUser._doc;
+
+            res.status(200).cookie('access_token', token,{
+                httpOnly:true
+            }).json(rest);
     }
     catch(error){
     next(error);
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,8 +1,9 @@
 import express from 'express'
-import { signup } from '../controllers/auth.controler.js';
+import { signup, signin } from '../controllers/auth.controler.js';
 
 const router = express.Router();
 
 router.post('/signup', signup)
+router.post('/signin', signin)
 
 export default router;
