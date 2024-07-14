b3d4ca78e4a4d1138b8121b2c8eecb59552c4aa6
Sundaram Seth <sundaram.s@eccentricengine.com>
2024-07-14 18:25:48 +0530

    complete signup page ui

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,54 @@
+import {Button, Label, TextInput} from 'flowbite-react'
+import { Link } from 'react-router-dom'
+
 export default function SignUp() {
   return (
-    <div>
-      SignUp
+    <div className="mt-20">
+      
+      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
+      {/*left*/}
+      <div className="text-left flex-1">
+        <p className="text-3xl font-semibold">Welcome to the Adhyatma</p>
+        <p className="text-sm my-3">This is a blogging site for Adhaytma</p>
+      </div>
+
+        {/*Right*/}
+      <div className="flex-1">
+       <form className='flex flex-col gap-4'>
+        <div className="">
+          <Label value='Your Username' />
+          <TextInput 
+          type='text'
+          placeholder='Username'
+          id='username'
+          />
+          <Label value='Your Email Adress' />
+          <TextInput 
+          type='text'
+          placeholder='name@company.com'
+          id='email'
+          />
+          <Label value='New Password' />
+          <TextInput 
+          type='text'
+          placeholder='Password'
+          id='password'
+          />
+        </div>
+
+        <Button gradientDuoTone='purpleToBlue' type='submit'>
+        Sign Up
+        </Button>
+        </form>
+        <div className='flex gap-2 text-sm mt-2'>
+          <span>Have an account?</span>
+          <Link to='/signin' className='text-blue-600 font-semibold'>
+          Sign In
+          </Link>
+        </div>
+      </div>
+      </div>
+
     </div>
   )
 }
