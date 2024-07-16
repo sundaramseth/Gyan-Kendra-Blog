d508abf7e432bc7521a7c3a1c35e29eecfd513ef
Sundaram Seth <sundaram.s@eccentricengine.com>
2024-07-16 19:02:13 +0530

    add user update api routes

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv'
 import userRoutes from './routes/users.route.js'
 import authRoutes from './routes/auth.route.js'
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
@@ -16,6 +17,7 @@ mongoose.connect(process.env.MANGO).then(
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.listen(3000, ()=>{
     console.log('Server is running on port 3000 !')
