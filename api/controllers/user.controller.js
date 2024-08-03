f5c652b7b05f168c8e3bb4ee9339eae841af2638
Sundaram Seth <sundaram.s@eccentricengine.com>
2024-08-03 23:23:03 +0530

    create getusers api routes

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -67,3 +67,51 @@ res.clearCookie('acess_token').status(200).json('User has been signed out Succes
  next(error)
 }
 }
+
+
+export const getUsers = async(req,res,next)=>{
+    if(!req.user.isAdmin){
+        return next(errorHandler(403,'You are not allowed to see all user'));
+    }
+    try {
+        const startIndex = parseInt(req.query.startIndex) || 0;
+        const limit = parseInt(req.query.limit) || 9;
+        const sortDirection = req.query.sort === 'asc' ? 1: -1;
+
+        const users = await User.find()
+        .sort({createdOn:sortDirection})
+        .skip(startIndex)
+        .limit(limit);
+
+        const usersWithoutPassword = users.map((user)=>{
+            const {password, ...rest} = user._doc;
+            return rest;
+        });
+
+        const totalUsers = await User.countDocuments();
+
+
+        const now = new Date();
+        
+        const oneMonthAgo = new Date(
+            now.getFullYear(),
+            now.getMonth()-1,
+            now.getDate()
+        );
+
+        const lastMonthUsers = await User.countDocuments({
+            createdAt:{$gte:oneMonthAgo},
+        });
+
+        res.status(200).json({
+            users:usersWithoutPassword,
+            totalUsers,
+            lastMonthUsers
+        });
+
+
+
+    } catch (error) {
+        next(error);
+    }
+}
--- api/routes/users.route.js
+++ api/routes/users.route.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { test } from '../controllers/user.controller.js';
-import {updateUser} from '../controllers/user.controller.js'
+import {updateUser, getUsers} from '../controllers/user.controller.js'
 import {deleteUser, signout} from '../controllers/user.controller.js'
 import { verifyToken } from '../utils/verifyUser.js';
 const router = express.Router();
@@ -9,5 +9,6 @@ router.get('/test', test)
 router.put('/update/:userId', verifyToken, updateUser)
 router.delete('/delete/:userId', verifyToken, deleteUser);
 router.post('/signout', signout);
+router.get('/getusers', verifyToken, getUsers);
 
 export default router;
